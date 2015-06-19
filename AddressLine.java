32dd23f5591ac873c16568f7e2cec39d997fbcfe
mjmoser <mjanelle720@hotmail.com>
2015-06-18 21:06:26 -0500

    ageQuestion

--- AddressLine.java
+++ AddressLine.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class AddressLine { 
 	public static void main(String[] args) {
-		System.out.println("Address Line 1");
-		System.out.println("Address Line 2");
-		System.out.println("Address Line 3");
+		String name;
+		int age; 
+		String top = "========================================";
+		Scanner kb = new Scanner(System.in);
+	
+		System.out.println("What is your name?");
+		name = kb.next();
+		
+		System.out.println("What is your age?");
+		age = kb.nextInt();
+		
+		System.out.println(top);
+		System.out.println("Wow " + name + ", you are really old!");
 	}
 }
